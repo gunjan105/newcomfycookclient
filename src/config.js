ef3e02ddb0f75d2d3a90fbd77f77146cf9d4b928
Ankit Modi <ankitmodi31198@gmail.com>
2020-02-22 23:06:44 +0530

    server path changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // server_path: 'http://localhost:4000'
-    server_path: 'http://192.168.137.1:4000'
+    server_path: 'http://comfycookserver.herokuapp.com'
 }
 
 export default config;
