6eb723ec8481567406a6a5a1e06c617e55e96daa
Jash Patel <jashdetroj@gmail.com>
2020-02-23 19:10:10 +0530

    minor changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // server_path: 'http://localhost:4000'
-    server_path: 'http://comfycookserver.herokuapp.com'
+    server_path: 'http://192.168.0.103:4000'
 }
 
 export default config;
